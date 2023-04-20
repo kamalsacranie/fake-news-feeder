dbb44a753bb62eaadd4fa25b69e7b2d298f265ef
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-04-20 21:58:05 +0100

    Add topics page which redirects to filtered articles page

--- src/pages/TopicsPage.tsx
+++ src/pages/TopicsPage.tsx
@@ -1,5 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { Hourglass } from "react95";
+import { getTopics, Topic } from "../api";
+import TopicListItem from "../components/TopicListItem";
+
 const TopicsPage = () => {
-  return 
+  const {
+    isLoading,
+    error,
+    data: topics,
+  } = useQuery<Topic[], Error>({
+    queryKey: ["topics"],
+    queryFn: getTopics,
+  });
+  if (isLoading) return <Hourglass />;
+  if (error) return <div>{error.message}</div>;
+  if (!topics) return <></>;
+  return (
+    <>
+      {topics.map((topic) => (
+        <TopicListItem key={topic.slug} topic={topic} />
+      ))}
+    </>
+  );
 };
 
 export default TopicsPage;
